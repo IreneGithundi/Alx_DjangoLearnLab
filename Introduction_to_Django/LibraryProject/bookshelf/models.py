635f953ab28a0f82b49d4a490a43a29cf791aabf
IreneGithundi <njokiirene4349@gmail.com>
2025-12-02 09:23:32 +0300

    CRUD OPERATIONS

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -5,3 +5,7 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    #string representation
+    def __str__(self):
+        return (f"ID:{self.id}: {self.title}, {self.author}, {self.publication_year}")
