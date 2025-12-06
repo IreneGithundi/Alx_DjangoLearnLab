93213d9253462754206e9b54545821619de08c17
IreneGithundi <njokiirene4349@gmail.com>
2025-12-06 20:04:56 +0300

    Django Relationships Model

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -33,7 +33,7 @@ def books_in_library(library_name):
 def librarian_of_library(library_name):
     try:
         library = Library.objects.get(name=library_name)
-        librarian = library.librarian
+        librarian = Librarian.objects.get(library=library)
 
         print(f"\nLibrarian of {library_name}: {librarian.name}")
 
